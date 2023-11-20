0d9f810b808af22d4394d02808b747f5611ab717
Will Medina <williams.medinaa@gmail.com>
2023-11-20 06:27:53 -0700

    fix: move all keymaps to remap file

--- lua/willyelm/remap.lua
+++ lua/willyelm/remap.lua
@@ -1,5 +1,12 @@
+-- Telescope 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files"  })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "File Grep" })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "File Buffers" })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Help Preview"})
+
+-- Outline
+vim.keymap.set("n", "<leader>tt", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+
+-- Tree
+vim.keymap.set("n", "<leader>ft", "<cmd>NvimTreeFocus<CR>", { desc = "Focus FileTree" })
